77fd2a7781f989f90f5f602af1ef497e19e5a50a
Matt Gibson <gothick@gothick.org.uk>
2022-10-29 18:53:14 +0100

    Add postcards

--- map.js
+++ map.js
@@ -2,6 +2,42 @@
  L
 */
 
+// Hardcoded for now as we're just trying this out with a few postcards.
+var postcards = [
+    {
+        "title": "Bristol Castle",
+        "description": "Replica of Bristol Castle. WH Smith postcard.",
+        "full": encodeURI("images/full/Postcard Bristol Castle.jpg"),
+        "small": encodeURI("images/small/Postcard Bristol Castle.jpg"),
+        "lat": 51.446483,
+        "lng": -2.625353
+    },
+    {
+        "title": "Clifton Bridge Station",
+        "description": "Later postcard of Clifton Bridge Station. The Portishead Branch Line still travels along this route, and bridges that marked each platform end are still in use today.",
+        "full": encodeURI("images/full/Postcard Clifton Bridge Station.jpg"),
+        "small": encodeURI("images/small/Postcard Clifton Bridge Station.jpg"),
+        "lat": 51.447155,
+        "lng": -2.625766
+    },
+    {
+        "title": "Figure Eight Coaster",
+        "description": "The Figure 8 Coaster, or Scenic Railway, an early roller coaster. WH Smith postcard.",
+        "full": encodeURI("images/full/Postcard Figure Eight Coaster.jpg"),
+        "small": encodeURI("images/small/Postcard Figure Eight Coaster.jpg"),
+        "lat": 51.442551,
+        "lng": -2.624538
+    },
+    {
+        "title": "Riverside Promenade",
+        "description": "Bandstand with Austrailian Pavilion behind. WH Smith postcard.",
+        "full": encodeURI("images/full/Postcard Riverside Promenade.jpg"),
+        "small": encodeURI("images/small/Postcard Riverside Promenade.jpg"),
+        "lat": 51.446014,
+        "lng": -2.623449
+    }
+];
+
 var mapMinZoom = 14;
 var mapMaxZoom = 19;
 
@@ -38,7 +74,7 @@ var modernLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.pn
 }).addTo(map);
 
 var attribution = 'Mashup by <a href="https://gothick.org.uk">gothick</a>.';
-attribution += '<br />Historical site plan (digitised and adjusted for north-facing friendliness): Bristol Archives, <a href="https://archives.bristol.gov.uk/records/37165/7/3">39864/1/1: Printed plan of Bristol International Exhibition Site adjoining Ashton Avenue and River Avon, 1914</a>.';
+attribution += '<br />Historical site plan (digitised &amp; adjusted for north-facing friendliness): Bristol Archives, <a href="https://archives.bristol.gov.uk/records/37165/7/3">39864/1/1: Printed plan of Bristol International Exhibition Site adjoining Ashton Avenue and River Avon, 1914</a>.';
 attribution += '<br />Tiled by <a href="https://gdal.org/programs/gdal2tiles.html">GDAL2Tiles</a> from <a href="https://gdal.org">GDAL</a> &amp; <a href="http://www.osgeo.org/">OSGeo</a> <a href="http://code.google.com/soc/">GSoC</a>.';
 attribution += '<br />Modern map &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>.';
 attribution += '<br />Buy Clive Burlton\'s book <em><a href="https://www.bristolbooks.org/shop/bristols-lost-city">Bristol\'s Lost City</a></em>.';
@@ -55,10 +91,29 @@ var layer = L.tileLayer('{z}/{x}/{y}.png', options).addTo(map);
 
 map.zoomControl.setPosition('bottomright');
 
+addPostcards(map, postcards);
+
 function mapOpacity(sliderVal) {
     'use strict';
     var hOpac = Math.min(sliderVal, 1.0);
     var mOpac = Math.min(2.0 - sliderVal, 1.0);
     layer.setOpacity(hOpac);
     modernLayer.setOpacity(mOpac);
+}
+
+
+function addPostcards(map, postcards) {
+    'use strict';
+    postcards.forEach(function (postcard) {
+        var marker = new L.Marker([postcard["lat"], postcard["lng"]]);
+        marker.bindPopup(
+            "<a href='" + postcard["full"] + "'><img width='320' src='" + postcard["small"] + "'/></a><p>" + postcard["description"] + "</p>",
+            {
+                maxWidth: "auto"
+            }
+        ).openPopup();
+
+        marker.addTo(map);
+
+    });
 }
