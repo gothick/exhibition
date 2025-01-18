a081481ee7bafa5ca2fde40828aa5cd52b6719b4
Matt Gibson <gothick@gothick.org.uk>
2025-01-18 17:19:22 +0000

    Upgrade capistrano lock version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock "~> 3.19.1"
 
 set :application, "exhibition"
 set :repo_url, "git@github.com:gothick/exhibition.git"
